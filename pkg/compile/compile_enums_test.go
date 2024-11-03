5104a384d43d2bdeecd966275223869887e6594c
bnunamak <brennan@nunamaker.de>
2024-11-03 11:05:28 +0100

    Added enum hook tests

--- pkg/compile/compile_enums_test.go
+++ pkg/compile/compile_enums_test.go
@@ -1,6 +1,7 @@
 package compile_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/kaloseia/morphe-go/pkg/yaml"
@@ -210,5 +211,183 @@ func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_EntryTypeMismatch(
 
 	suite.ErrorContains(tsEnumErr, "enum entry 'Blue' value 'rgb(0,0,255)' with type 'string' does not match the enum type of 'Integer'")
 
+	suite.Nil(tsEnum)
+}
+
+func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_StartHook_Successful() {
+	var featureFlag = "otherName"
+	enumHooks := hook.CompileMorpheEnum{
+		OnCompileMorpheEnumStart: func(config cfg.MorpheEnumsConfig, enum yaml.Enum) (cfg.MorpheEnumsConfig, yaml.Enum, error) {
+			if featureFlag != "otherName" {
+				return config, enum, nil
+			}
+			enum.Name = enum.Name + "CHANGED"
+			delete(enum.Entries, "Green")
+			return config, enum, nil
+		},
+	}
+
+	enumsConfig := cfg.MorpheEnumsConfig{}
+
+	enum0 := yaml.Enum{
+		Name: "Color",
+		Type: yaml.EnumTypeString,
+		Entries: map[string]any{
+			"Red":   "rgb(255,0,0)",
+			"Green": "rgb(0,255,0)",
+			"Blue":  "rgb(0,0,255)",
+		},
+	}
+
+	tsEnum, tsEnumErr := compile.MorpheEnumToTsEnum(enumHooks, enumsConfig, enum0)
+
+	suite.Nil(tsEnumErr)
+
+	suite.Equal(tsEnum.Name, "ColorCHANGED")
+	suite.Equal(tsEnum.Type, tsdef.TsTypeString)
+
+	tsEntries0 := tsEnum.Entries
+	suite.Len(tsEntries0, 2)
+
+	tsEntry00 := tsEntries0[0]
+	suite.Equal(tsEntry00.Name, "Blue")
+	suite.Equal(tsEntry00.Value, "rgb(0,0,255)")
+
+	tsEntry01 := tsEntries0[1]
+	suite.Equal(tsEntry01.Name, "Red")
+	suite.Equal(tsEntry01.Value, "rgb(255,0,0)")
+}
+
+func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_StartHook_Failure() {
+	var featureFlag = "otherName"
+	enumHooks := hook.CompileMorpheEnum{
+		OnCompileMorpheEnumStart: func(config cfg.MorpheEnumsConfig, enum yaml.Enum) (cfg.MorpheEnumsConfig, yaml.Enum, error) {
+			if featureFlag != "otherName" {
+				return config, enum, nil
+			}
+			return config, enum, fmt.Errorf("compile enum start hook error")
+		},
+	}
+
+	enumsConfig := cfg.MorpheEnumsConfig{}
+
+	enum0 := yaml.Enum{
+		Name: "Color",
+		Type: yaml.EnumTypeString,
+		Entries: map[string]any{
+			"Red":   "rgb(255,0,0)",
+			"Green": "rgb(0,255,0)",
+			"Blue":  "rgb(0,0,255)",
+		},
+	}
+
+	tsEnum, tsEnumErr := compile.MorpheEnumToTsEnum(enumHooks, enumsConfig, enum0)
+
+	suite.ErrorContains(tsEnumErr, "compile enum start hook error")
+	suite.Nil(tsEnum)
+}
+
+func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_SuccessHook_Successful() {
+	var featureFlag = "otherName"
+	enumHooks := hook.CompileMorpheEnum{
+		OnCompileMorpheEnumSuccess: func(enum *tsdef.Enum) (*tsdef.Enum, error) {
+			if featureFlag != "otherName" {
+				return enum, nil
+			}
+			enum.Name = enum.Name + "CHANGED"
+			newEntries := []tsdef.EnumEntry{}
+			for _, enumEntry := range enum.Entries {
+				if enumEntry.Name == "Green" {
+					continue
+				}
+				newEntries = append(newEntries, enumEntry)
+			}
+			enum.Entries = newEntries
+			return enum, nil
+		},
+	}
+
+	enumsConfig := cfg.MorpheEnumsConfig{}
+
+	enum0 := yaml.Enum{
+		Name: "Color",
+		Type: yaml.EnumTypeString,
+		Entries: map[string]any{
+			"Red":   "rgb(255,0,0)",
+			"Green": "rgb(0,255,0)",
+			"Blue":  "rgb(0,0,255)",
+		},
+	}
+
+	tsEnum, tsEnumErr := compile.MorpheEnumToTsEnum(enumHooks, enumsConfig, enum0)
+
+	suite.Nil(tsEnumErr)
+
+	suite.Equal(tsEnum.Name, "ColorCHANGED")
+	suite.Equal(tsEnum.Type, tsdef.TsTypeString)
+
+	tsEntries0 := tsEnum.Entries
+	suite.Len(tsEntries0, 2)
+
+	tsEntry00 := tsEntries0[0]
+	suite.Equal(tsEntry00.Name, "Blue")
+	suite.Equal(tsEntry00.Value, "rgb(0,0,255)")
+
+	tsEntry01 := tsEntries0[1]
+	suite.Equal(tsEntry01.Name, "Red")
+	suite.Equal(tsEntry01.Value, "rgb(255,0,0)")
+}
+
+func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_SuccessHook_Failure() {
+	var featureFlag = "otherName"
+	enumHooks := hook.CompileMorpheEnum{
+		OnCompileMorpheEnumSuccess: func(enum *tsdef.Enum) (*tsdef.Enum, error) {
+			if featureFlag != "otherName" {
+				return enum, nil
+			}
+			return nil, fmt.Errorf("compile enum success hook error")
+		},
+	}
+
+	enumsConfig := cfg.MorpheEnumsConfig{}
+
+	enum0 := yaml.Enum{
+		Name: "Color",
+		Type: yaml.EnumTypeString,
+		Entries: map[string]any{
+			"Red":   "rgb(255,0,0)",
+			"Green": "rgb(0,255,0)",
+			"Blue":  "rgb(0,0,255)",
+		},
+	}
+
+	tsEnum, tsEnumErr := compile.MorpheEnumToTsEnum(enumHooks, enumsConfig, enum0)
+
+	suite.ErrorContains(tsEnumErr, "compile enum success hook error")
+	suite.Nil(tsEnum)
+}
+
+func (suite *CompileEnumsTestSuite) TestMorpheEnumToTsObjects_FailureHook() {
+	enumHooks := hook.CompileMorpheEnum{
+		OnCompileMorpheEnumFailure: func(config cfg.MorpheEnumsConfig, enum yaml.Enum, compileFailure error) error {
+			return fmt.Errorf("Enum %s: %w", enum.Name, compileFailure)
+		},
+	}
+
+	enumsConfig := cfg.MorpheEnumsConfig{}
+
+	enum0 := yaml.Enum{
+		Name: "Color",
+		Type: "",
+		Entries: map[string]any{
+			"Red":   "rgb(255,0,0)",
+			"Green": "rgb(0,255,0)",
+			"Blue":  "rgb(0,0,255)",
+		},
+	}
+
+	tsEnum, tsEnumErr := compile.MorpheEnumToTsEnum(enumHooks, enumsConfig, enum0)
+
+	suite.ErrorContains(tsEnumErr, "Enum Color: morphe enum has no type")
 	suite.Nil(tsEnum)
 }
